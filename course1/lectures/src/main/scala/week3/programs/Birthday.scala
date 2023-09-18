997f4f251bebbc5f4764ef80c52d42b01bcfcef3
Manuel Martinez <manmartgarc@gmail.com>
2023-09-17 18:20:18 -0700

    feat: add lecture 3 notes

---
+++ course1/lectures/src/main/scala/week3/programs/Birthday.scala
@@ -0,0 +1,4 @@
+package week3.programs
+
+@main def birthday(name: String, age: Int) =
+  println(s"Happy birthday $name! $age years old already!")
---
+++ course1/lectures/src/main/scala/week3/programs/Hello.scala
@@ -0,0 +1,4 @@
+package week3.programs
+
+object Hello:
+  def main(args: Array[String]): Unit = println("Hello, World!")
